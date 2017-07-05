e6d2d5936db6ec2cff8f02c66b9dc916da4f50bb
Peter Forsythe <pforsythe@vistaprint.com>
2017-07-05 16:40:23 +0200

    clean up variables

--- apps/ec2_standalone/variables.tf
+++ apps/ec2_standalone/variables.tf
@@ -5,18 +5,6 @@ variable "account_type" {
   default = "sandbox"
 }
 
-variable "app_name" {
-  description = "A value to append to the EC2 identifer to create a unique name"
-  default = "ec2-instance"
-}
-
-variable "number_servers" {
-  default = 1
-}
-variable "enabled" {
-  default = 1
-}
-
 variable "user_data" {
   default = ""
 }
@@ -29,44 +17,33 @@ variable "ami_id" {
 variable "instance_type" {
   default = "t2.nano"
 }
-variable "application_name" {
-  default = "application_name Change this"
+variable "number_servers" {
+  default = 1
 }
+variable "associtate_public_id"{
+  default = false
+}
+variable "subnet_id" {}
+
 ## Tags
-variable "description" {}
-variable "environment" {}
 variable "tags_business_unit" {
   default = "Albumprinter"
 }
-variable "tags_cost_center" {
-  default = "CC443100"
-}
+variable "tags_purpose" {}
+variable "tags_cost_center" {}
 variable "tags_team" {}
-variable "tags_purpose" {
-  default = "development"
-}
-
 variable "tags_name" {}
+variable "tags_contact" {}
+
+### Used in tags...and other places
+variable "description" {}
+variable "environment" {}
+variable "application_name" {}
 
 ## Security group
 variable "sg_name" {}
-### Security group rule
-//variable "ingress_protocol" {}
-//variable "ingress_to_port" {}
-//variable "ingress_from_port" {}
-//variable "egress_protocol" {}
-//variable "egress_to_port" {}
-//variable "egress_from_port" {}
-
 variable "sg_description" {}
 
 ## Keys
 variable "key_name" {}
 variable "public_key" {}
-
-#
-variable "subnet_id" {}
-
-variable "associtate_public_id"{
-  default = false
-}
