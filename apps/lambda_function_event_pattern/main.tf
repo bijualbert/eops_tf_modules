91307a031419d3d8076ecc47b437584fe961da66
zoran.cikojevic <zcikojevic@albelli.com>
2017-07-28 16:14:15 +0200

    remove data srouce dependancy

--- apps/lambda_function_event_pattern/main.tf
+++ apps/lambda_function_event_pattern/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 //  profile = "eops-sandbox"
 }
 
-module "aws_core_data" {
-  source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
-  // For local development use instead:
-  //  source = "../../data_providers/aws_account_core_data"
-}
+//module "aws_core_data" {
+//  source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
+//  // For local development use instead:
+//  //  source = "../../data_providers/aws_account_core_data"
+//}
