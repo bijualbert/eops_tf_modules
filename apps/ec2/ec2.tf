5d4cc9284ab5b5fd5d0ad359dd30fd9881ff625d
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-08-28 09:37:57 +0200

    add instance count

--- apps/ec2/ec2.tf
+++ apps/ec2/ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "app" {
   ami = "${var.ami}"
+  count = "${var.instance_count}"
   instance_type = "${var.instance_type}"
   associate_public_ip_address = "${var.private == true ? false :true}"
   iam_instance_profile = "${aws_iam_instance_profile.app.name}"
@@ -17,22 +18,6 @@ resource "aws_instance" "app" {
     AutoOn          = "${var.tags_AutoOn}"
     AutoOff         = "${var.tags_AutoOff}"
     AutoOnDays      = "${var.tags_AutoOnDays}"
-  }
-}
-
-# SG for instance
-resource "aws_security_group" "app" {
-  name        = "SG-${var.app_name}"
-  description = "Allow inbound traffic for ${var.app_name}"
-  vpc_id      = "${module.aws_core_data.vpc_id}"
-  tags {
-    Name            = "${var.app_name}"
-    "Business Unit" = "${var.tags_business_unit}"
-    "Cost Center"   = "${var.tags_cost_center}"
-    Team            = "${var.tags_team}"
-    Purpose         = "${var.tags_purpose}"
-    Description     = "${var.description}"
-    Environment     = "${var.environment}"
   }
 }
 
