1ad2d04a185226d545f1784ec93a15c6408e09fb
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-08-28 14:17:20 +0200

    add AZs

--- apps/ec2_asg/asg.tf
+++ apps/ec2_asg/asg.tf
@@ -10,6 +10,7 @@ resource "aws_autoscaling_group" "asg" {
   max_size                  = "${var.max_size}"
   min_size                  = "${var.min_size}"
   name                      = "${var.app_name}"
+  availability_zones  = ["eu-west-1b", "eu-west-1a"]
 //  roles                     = ["${aws_iam_role.app.id}"]
 
   //  count = "${length(var.min_elb_capacity) > 0 || length(var.wait_for_elb_capacity) > 0 ? 0 : 1}"
