7b314f6872bd5ea1f01ab9d5d4c3ecfa99f65c4c
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-08-28 09:39:28 +0200

    re-add security groups shizzel

--- apps/ec2/ec2.tf
+++ apps/ec2/ec2.tf
@@ -21,6 +21,22 @@ resource "aws_instance" "app" {
   }
 }
 
+# SG for instance
+resource "aws_security_group" "app" {
+  name        = "SG-${var.app_name}"
+  description = "Allow inbound traffic for ${var.app_name}"
+  vpc_id      = "${module.aws_core_data.vpc_id}"
+  tags {
+    Name            = "${var.app_name}"
+    "Business Unit" = "${var.tags_business_unit}"
+    "Cost Center"   = "${var.tags_cost_center}"
+    Team            = "${var.tags_team}"
+    Purpose         = "${var.tags_purpose}"
+    Description     = "${var.description}"
+    Environment     = "${var.environment}"
+  }
+}
+
 resource "aws_iam_instance_profile" "app" {
   name = "${var.app_name}"
   role = "${aws_iam_role.app.id}"
