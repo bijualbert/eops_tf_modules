d3705796c10ec9c857e19f78c4e2b43f28faff80
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-08-29 11:21:12 +0200

    restore SG tags as they were not the issue.

--- apps/autoscaling_group/asg.tf
+++ apps/autoscaling_group/asg.tf
@@ -38,15 +38,15 @@ resource "aws_security_group" "asg" {
   name        = "SG-${var.app_name}"
   description = "Allow inbound traffic for ${var.app_name}"
   vpc_id      = "${module.aws_core_data.vpc_id}"
-//  tags {
-//    Name            = "${var.app_name}"
-//    "Business Unit" = "${var.tags_business_unit}"
-//    "Cost Center"   = "${var.tags_cost_center}"
-//    Team            = "${var.tags_team}"
-//    Purpose         = "${var.tags_purpose}"
-//    Description     = "${var.description}"
-//    Environment     = "${var.environment}"
-//  }
+  tags {
+    Name            = "${var.app_name}"
+    "Business Unit" = "${var.tags_business_unit}"
+    "Cost Center"   = "${var.tags_cost_center}"
+    Team            = "${var.tags_team}"
+    Purpose         = "${var.tags_purpose}"
+    Description     = "${var.description}"
+    Environment     = "${var.environment}"
+  }
 }
 
 resource "aws_iam_instance_profile" "asg" {
