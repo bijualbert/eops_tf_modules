cabb937fae3f731f2427ac1bb6971043e73b03c7
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-08-29 09:31:05 +0200

    move security definition into EC2 definition

--- apps/ec2/ec2_security_group.tf
+++ apps/ec2/ec2_security_group.tf
@@ -1,40 +1,40 @@
-# SG for instance
-resource "aws_security_group" "app" {
-  name        = "EC2-${var.app_name}"
-  description = "Allow inbound traffic for ${var.app_name}"
-  vpc_id      = "${module.aws_core_data.vpc_id}"
-  tags {
-    Name            = "${var.app_name}"
-    "Business Unit" = "${var.tags_business_unit}"
-    "Cost Center"   = "${var.tags_cost_center}"
-    Team            = "${var.tags_team}"
-    Purpose         = "${var.tags_purpose}"
-    Description     = "${var.description}"
-    Environment     = "${var.environment}"
-  }
-}
-
-resource "aws_iam_instance_profile" "app" {
-  name = "EC2-${var.app_name}"
-  roles = ["${aws_iam_role.app.id}"]
-}
-
-resource "aws_iam_role" "app" {
-  name = "EC2-${var.app_name}"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
+//# SG for instance
+//resource "aws_security_group" "app" {
+//  name        = "EC2-${var.app_name}"
+//  description = "Allow inbound traffic for ${var.app_name}"
+//  vpc_id      = "${module.aws_core_data.vpc_id}"
+//  tags {
+//    Name            = "${var.app_name}"
+//    "Business Unit" = "${var.tags_business_unit}"
+//    "Cost Center"   = "${var.tags_cost_center}"
+//    Team            = "${var.tags_team}"
+//    Purpose         = "${var.tags_purpose}"
+//    Description     = "${var.description}"
+//    Environment     = "${var.environment}"
+//  }
+//}
+//
+//resource "aws_iam_instance_profile" "app" {
+//  name = "EC2-${var.app_name}"
+//  roles = ["${aws_iam_role.app.id}"]
+//}
+//
+//resource "aws_iam_role" "app" {
+//  name = "EC2-${var.app_name}"
+//
+//  assume_role_policy = <<EOF
+//{
+//  "Version": "2012-10-17",
+//  "Statement": [
+//    {
+//      "Action": "sts:AssumeRole",
+//      "Principal": {
+//        "Service": "ec2.amazonaws.com"
+//      },
+//      "Effect": "Allow",
+//      "Sid": ""
+//    }
+//  ]
+//}
+//EOF
+//}
