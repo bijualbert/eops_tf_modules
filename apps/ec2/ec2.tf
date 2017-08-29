a4a953643c13aa0f4bf6a2a8e0c7743f7b2e6265
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-08-29 15:46:23 +0200

    remove if for private ec2 address

--- apps/ec2/ec2.tf
+++ apps/ec2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "app" {
   ami                = "${var.ami}"
   count               = "${var.instance_count}"
   instance_type       = "${var.instance_type}"
-  associate_public_ip_address = "${var.private == true ? false :true}"
+  associate_public_ip_address = "${var.private}"
   iam_instance_profile = "${aws_iam_instance_profile.app.id}"
   security_groups     = ["${aws_security_group.app.id}"]
   subnet_id           = "${element(module.aws_core_data.private_subnets,count.index)}"
