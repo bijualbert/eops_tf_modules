c010a26edf28aa43261a243df4e76e6c64116de0
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-09-06 13:41:00 +0200

    fix type

--- apps/vpc_peering/vpc_peering_connection.tf
+++ apps/vpc_peering/vpc_peering_connection.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_peering_connection" "vpc_peering" {
-    peer_vpc_id   = "${{ecom1_vpc_id}"
+    peer_vpc_id   = "${ecom1_vpc_id}"
     vpc_id        = "${db_vpc_id}"
 
     accepter {
