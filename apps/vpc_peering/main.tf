a066d5aaf4cc5449438135110f19339aebe577e9
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-09-07 14:03:17 +0200

    make it a list

--- apps/vpc_peering/main.tf
+++ apps/vpc_peering/main.tf
@@ -28,7 +28,7 @@ data "aws_route_table" "db_vpc_route_table" {
   provider = "aws.db-sb"
   filter {
     name   = "VPC"
-    values = "${data.aws_vpc.db_vpc_main.id}"
+    values = ["${data.aws_vpc.db_vpc_main.id}"]
   }
 }
 
