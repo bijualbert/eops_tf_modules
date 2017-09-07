acefde1e221a1deb37a1d6282f527eb3e0c5bee9
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-09-07 14:52:18 +0200

    reduce filter - give me something back FFS

--- apps/vpc_peering/main.tf
+++ apps/vpc_peering/main.tf
@@ -27,10 +27,10 @@ data "aws_vpc" "db_vpc_main" {
 data "aws_route_table" "db_vpc_route_table" {
   provider = "aws.db-sb"
 
-  filter {
-    name = "vpc-id"
-    values = ["${data.aws_vpc.db_vpc_main.id}"]
-  }
+//  filter {
+//    name = "vpc-id"
+//    values = ["${data.aws_vpc.db_vpc_main.id}"]
+//  }
   filter {
     name = "association.main"
     values = [true]
@@ -57,10 +57,10 @@ data "aws_vpc" "ecom1_vpc_main" {
 data "aws_route_table" "ecom1_vpc_route_table" {
   provider = "aws.ecom1-sb"
 
-  filter {
-    name = "vpc-id"
-    values = ["${data.aws_vpc.ecom1_vpc_main.id}"]
-  }
+//  filter {
+//    name = "vpc-id"
+//    values = ["${data.aws_vpc.ecom1_vpc_main.id}"]
+//  }
   filter {
     name = "association.main"
     values = [true]
