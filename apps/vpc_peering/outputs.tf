4020cb25ecd3f3811e2eb990816a00a5af09f6e8
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-09-07 11:14:57 +0200

    remove output

--- apps/vpc_peering/outputs.tf
+++ apps/vpc_peering/outputs.tf
@@ -1,6 +1,6 @@
-output "ecom1_vpc_id" {
-  value = "${data.aws_vpc.ecom1_vpc_id.id}"
-}
-output "db_vpc_id" {
-  value = "${data.aws_vpc.db_vpc_id.id}"
-}
+//output "ecom1_vpc_id" {
+//  value = "${data.aws_vpc.ecom1_vpc_id.id}"
+//}
+//output "db_vpc_id" {
+//  value = "${data.aws_vpc.db_vpc_id.id}"
+//}
