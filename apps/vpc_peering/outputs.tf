8bcc1ea5d970d2ec9f3335a978bdb0120ce08042
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-09-08 14:04:09 +0200

    remove unused output

--- apps/vpc_peering/outputs.tf
+++ apps/vpc_peering/outputs.tf
@@ -5,7 +5,3 @@ output "ecom1_vpc_id" {
 output "db_vpc_id" {
   value = "${data.aws_vpc.db_vpc_main.id}"
 }
-
-output "peer_connection_id" {
-  value = "${aws_vpc_peering_connection.vpc_peering.id}"
-}
