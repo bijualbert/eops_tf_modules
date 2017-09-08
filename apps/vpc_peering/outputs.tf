0e22a378fa96b80bb361a34ed65e455cd84e3bb3
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-09-08 13:52:11 +0200

    and outputs

--- apps/vpc_peering/outputs.tf
+++ apps/vpc_peering/outputs.tf
@@ -2,17 +2,17 @@ output "ecom1_vpc_id" {
   value = "${data.aws_vpc.ecom1_vpc_main.id}"
 }
 
-output "ecom1_vpc_route_table" {
-  value = "${data.aws_route_table.ecom1_vpc_route_table.id}"
-}
+//output "ecom1_vpc_route_table" {
+//  value = "${data.aws_route_table.ecom1_vpc_route_table.id}"
+//}
 
 output "db_vpc_id" {
   value = "${data.aws_vpc.db_vpc_main.id}"
 }
 
-output "db_vpc_route_table" {
-  value = "${data.aws_route_table.db_vpc_route_table.id}"
-}
+//output "db_vpc_route_table" {
+//  value = "${data.aws_route_table.db_vpc_route_table.id}"
+//}
 
 output "peer_connection_id" {
   value = "${aws_vpc_peering_connection.vpc_peering.id}"
