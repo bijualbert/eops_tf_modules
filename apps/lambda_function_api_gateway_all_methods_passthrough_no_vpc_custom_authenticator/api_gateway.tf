77aac791d25b69bade5a5503770b846a332734c1
Martijn Mol <martijn.mol@albumprinter.com>
2017-10-05 16:26:38 +0200

    WATSON-807

--- apps/lambda_function_api_gateway_all_methods_passthrough_no_vpc_custom_authenticator/api_gateway.tf
+++ apps/lambda_function_api_gateway_all_methods_passthrough_no_vpc_custom_authenticator/api_gateway.tf
@@ -22,6 +22,7 @@ resource "aws_api_gateway_method" "app" {
   }
 }
 
+
 resource "aws_api_gateway_method_response" "200" {
   rest_api_id = "${aws_api_gateway_rest_api.app.id}"
   resource_id = "${aws_api_gateway_resource.app.id}"
