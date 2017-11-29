3472009365fe823220296aaf5a93c642e6fd4f49
Martijn Mol <martijn.mol@albumprinter.com>
2017-11-29 13:51:51 +0100

    Add SNS topic subscription

--- apps/lambda_function_sns/sns.tf
+++ apps/lambda_function_sns/sns.tf
@@ -1,3 +1,9 @@
 resource "aws_sns_topic" "lambda_replay_sns" {
   name = "${var.app_name}_REPLAY"
 }
+
+resource "aws_sns_topic_subscription" "lambda_replay_topic_subscription" {
+  topic_arn = "${aws_sns_topic.lambda_replay_sns.arn}"
+  protocol  = "lambda"
+  endpoint  = "${aws_lambda_function.app.arn}"
+}
