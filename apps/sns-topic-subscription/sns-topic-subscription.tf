36790ab9ed2515e878cd01499761a0bd01db1635
Peter Forsythe <peter.forsythe@albumprinter.com>
2017-12-04 16:38:57 +0100

    add resource for alert-subs

--- apps/sns-topic-subscription/sns-topic-subscription.tf
+++ apps/sns-topic-subscription/sns-topic-subscription.tf
@@ -3,3 +3,9 @@ resource "aws_sns_topic_subscription" "topic-subscription" {
   protocol  = "${var.protocol}"
   endpoint  = "${var.endpoint}"
 }
+
+resource "aws_sns_topic_subscription" "alert-subscription" {
+  topic_arn = "${var.topic_arn}"
+  protocol  = "${var.protocol}"
+  endpoint  = "${var.endpoint}"
+}
