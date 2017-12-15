f02ff103540e33add7aa67c02726a45aca0201e6
gmariano <giuseppe.mariano@albelli.com>
2017-12-15 15:25:33 +0100

    WATSON-935 Add sqs_consume_sns

---
+++ apps/sqs_consume_sns/sqs.tf
@@ -0,0 +1,83 @@
+resource "aws_sqs_queue" "sqs_queue" {
+  name                      = "${var.app_name}"
+  message_retention_seconds = 1209600
+  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.sqs_error_queue.arn}\",\"maxReceiveCount\":${var.redrive_policy.retry_count}}"
+}
+
+resource "aws_sqs_queue_policy" "sqs_sns_policy" {
+  queue_url = "${aws_sqs_queue.sqs_queue.id}"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.sqs_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${var.sns_subscription_arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_sqs_queue_policy" "sqs_sns_replay_policy" {
+  queue_url = "${aws_sqs_queue.sqs_queue.id}"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.sqs_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic_subscription.replay_topic_subscription.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_sqs_queue" "sqs_error_queue" {
+  name                      = "${var.app_name}-ERROR"
+  message_retention_seconds = 1209600
+}
+
+resource "aws_sqs_queue_policy" "sqs_error_queue_policy" {
+  queue_url = "${aws_sqs_queue.sqs_error_queue.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.sqs_error_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sqs_queue.sqs_queue.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
