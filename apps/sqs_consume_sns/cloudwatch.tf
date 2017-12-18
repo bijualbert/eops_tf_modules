189f190b8a866460002b1ff9c66b1929b21ba619
Unknown <antonio.neto@albelli.com>
2017-12-18 23:30:04 +0100

    WATSON-935 Change name and description of alarm

--- apps/sqs_consume_sns/cloudwatch.tf
+++ apps/sqs_consume_sns/cloudwatch.tf
@@ -1,5 +1,5 @@
-resource "aws_cloudwatch_metric_alarm" "foobar" {
-  alarm_name                = "${var.app_name}-ERROR-ALARM"
+resource "aws_cloudwatch_metric_alarm" "sqs_error_queue_alarm" {
+  alarm_name                = "${var.app_name}-ERROR-QUEUE-ALARM"
   comparison_operator       = "GreaterThanThreshold"
   evaluation_periods        = "1"
   metric_name               = "ApproximateNumberOfMessagesVisible"
@@ -7,9 +7,9 @@ resource "aws_cloudwatch_metric_alarm" "foobar" {
   period                    = "300"
   statistic                 = "Sum"
   threshold                 = "0"
-  alarm_description         = "This metric monitors messages on error queue"
+  alarm_description         = "This alarm monitors messages on error queue"
   alarm_actions             = ["${var.sns_alert_arn}"]
-  count                     = "${var.environment == "prod" ? 1 : 0 }"
+  count                     = "${var.environment == "test" ? 1 : 0 }"
   dimensions {
       QueueName = "${aws_sqs_queue.sqs_error_queue.name}"
     }
