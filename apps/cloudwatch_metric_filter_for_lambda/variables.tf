b74c789e927864cd3ab807a33ec92945741d9578
Peter Forsythe <peter.forsythe@albumprinter.com>
2018-01-10 16:06:04 +0100

    new filter

--- apps/cloudwatch_metric_filter_for_lambda/variables.tf
+++ apps/cloudwatch_metric_filter_for_lambda/variables.tf
@@ -21,5 +21,5 @@ variable "app_name" {}
 variable "environment" {}
 
 variable "pattern" {
-  default = "report_name = "REPORT", request_id_name = "RequestId:", request_id_value, duration_name = "Duration:", duration_value, duration_unit = "ms", billed_duration_name_1 = "Billed", bill_duration_name_2 = "Duration:", billed_duration_value, billed_duration_unit = "ms", memory_size_name_1 = "Memory", memory_size_name_2 = "Size:", memory_size_value, memory_size_unit = "MB", max_memory_used_name_1 = "Max", max_memory_used_name_2 = "Memory", max_memory_used_name_3 = "Used:", max_memory_used_value, max_memory_used_unit = "MB"]"
+  default = "[type=REPORT,...,MemUsed,MemUnit]"
 }
