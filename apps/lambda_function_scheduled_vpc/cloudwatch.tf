3fa1552acdb7dc6b13ee62b2c51ee159651140bd
Martijn Mol <martijn.mol@albumprinter.com>
2018-01-15 14:48:41 +0100

    WATSON-968 add cloud watch memory filter to default lambda function

--- apps/lambda_function_scheduled_vpc/cloudwatch.tf
+++ apps/lambda_function_scheduled_vpc/cloudwatch.tf
@@ -10,3 +10,10 @@ resource "aws_cloudwatch_event_target" "lambda_vpc" {
   arn       = "${aws_lambda_function.app.arn}"
   count     = "${var.enabled}"
 }
+
+module "lambda_app_cloudwatch" {
+  source = "git@github.com:albumprinter/eops_tf_modules.git//apps/cloudwatch_metric_filter_for_lambda?ref=WATSON-968-2"
+  app_name = "${var.app_name}"
+  log_group_name = "/aws/lambda/${var.app_name}"
+  alarm_threshold = "${var.lambda_memory_alert_threshold}"
+}
