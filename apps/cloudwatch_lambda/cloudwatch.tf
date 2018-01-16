d911de68ec1c303fefb11ed938ecb5df888a4205
Peter Forsythe <peter.forsythe@albumprinter.com>
2018-01-16 17:12:06 +0100

    WTSON-968 - create lambda CW solution

---
+++ apps/cloudwatch_lambda/cloudwatch.tf
@@ -0,0 +1,36 @@
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name = "/aws/lambda/${var.app_name}"
+  retention_in_days = "${var.retention_days}"
+
+  tags {
+    Environment = "${var.environment}"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "lambda_memory_metric" {
+  name = "${var.app_name}_memory_metric_filter"
+  pattern = "${var.pattern}"
+  log_group_name = "${var.log_group_name}"
+
+  metric_transformation {
+    name = "lambda_memory"
+    namespace = "Albelli"
+    value = "$MemUsed"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_memory_alert" {
+  alarm_name                = "${var.app_name}_memory_alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  datapoints_to_alarm       = "1"
+  metric_name               = "lambda_memory"
+  namespace                 = "Albelli"
+  period                    = "${var.period}"
+  statistic                 = "Maximum"
+  threshold                 = "${var.alarm_threshold}"
+  alarm_description         = "This metric alerts on memory usage"
+  insufficient_data_actions = []
+  alarm_actions = ["${var.alarm_action_arn}"]
+  actions_enabled = "${var.action_enabled}"
+}
