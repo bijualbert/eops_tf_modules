6d1895715c42c4aef5e83ef156fda681e9d31722
Martijn Mol <martijn.mol@albumprinter.com>
2018-01-17 15:10:04 +0100

    WATSON-968 use cloudwatch_lambda as preparation for future refactoring

--- apps/lambda_function_sns/cloudwatch.tf
+++ apps/lambda_function_sns/cloudwatch.tf
@@ -1,16 +1,10 @@
 module "lambda_app_cloudwatch" {
-  source = "git@github.com:albumprinter/eops_tf_modules.git//apps/cloudwatch_metric_filter_for_lambda?ref=WATSON-968"
+  source = "git@github.com:albumprinter/eops_tf_modules.git//apps/cloudwatch_lambda?ref=WATSON-968"
   app_name = "${var.app_name}"
   log_group_name = "/aws/lambda/${var.app_name}"
   alarm_threshold = "${var.lambda_memory_alert_threshold}"
   alarm_action_arn = "${var.alarm_action_arn}"
-}
-
-resource "aws_cloudwatch_log_group" "app_log_group" {
-  name = "/aws/lambda/${var.app_name}"
-  retention_in_days = "${var.retention_days}"
-
-  tags {
-    Environment = "${var.environment}"
-  }
+  environment = "${var.environment}"
+  tags_team = "${var.tags_team}"
+  description = "${var.description}"
 }
