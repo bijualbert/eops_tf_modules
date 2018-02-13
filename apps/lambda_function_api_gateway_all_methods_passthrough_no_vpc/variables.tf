1cfe4cdcbff02820c348fd33c6530febe08d5578
gmariano <giuseppe.mariano@albelli.com>
2018-02-13 11:08:23 +0100

    WATSON-1090 Add cloudwatch in the api templates where missing

--- apps/lambda_function_api_gateway_all_methods_passthrough_no_vpc/variables.tf
+++ apps/lambda_function_api_gateway_all_methods_passthrough_no_vpc/variables.tf
@@ -50,6 +50,9 @@ variable "response_template" {
   }
 }
 
+variable "lambda_memory_alert_threshold" {} 
+variable "alarm_action_arn" {} 
+
 variable "iam_policy_document" {
   default =<<EOF
 {
@@ -110,6 +113,10 @@ variable "iam_policy_document" {
     ]
 }
 EOF
+}
+
+variable "retention_days" { 
+  default = "30" 
 }
 
 variable "binary_media_types" {
