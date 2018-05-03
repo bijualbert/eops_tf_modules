a1d00b323d8d07da069f2bf83140cf362d34b297
Martijn Mol <martijn.mol@albumprinter.com>
2018-05-03 13:53:02 +0200

    MARIO-1299 remove TF Warning

--- apps/lambda_function_sns/outputs.tf
+++ apps/lambda_function_sns/outputs.tf
@@ -2,14 +2,12 @@ output "security_group_id" {
   value = "${aws_security_group.sg_for_app.id}"
 }
 output "lambda_kms_key_id" {
-  value = "${aws_lambda_function.app.id}"
+  value = "${aws_lambda_function.app.*.id}"
 }
 output "lambda_arn" {
-  value = "${aws_lambda_function.app.arn}"
+  value = "${aws_lambda_function.app.*.arn}"
 }
-output "lambda_kms_key_arn" {
-  value = "${aws_lambda_function.app.kms_key_arn}"
-}
+
 output "iam_role_arn" {
-  value = "${aws_iam_role.iam_for_app.arn}"
+  value = "${aws_iam_role.iam_for_app.*.arn}"
 }
