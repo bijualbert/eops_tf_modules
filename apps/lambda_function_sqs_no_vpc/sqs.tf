2553f70d76a9f0e4a9579c38a92a8827b486ef9b
gmariano <giuseppe.mariano@albelli.com>
2018-08-15 18:10:58 +0200

    LUIGi-11 Invert queues order

--- apps/lambda_function_sqs_no_vpc/sqs.tf
+++ apps/lambda_function_sqs_no_vpc/sqs.tf
@@ -1,4 +1,38 @@
 
+#ERROR QUEUE
+resource "aws_sqs_queue" "sqs_error_queue" {
+  name                        = "${var.sqs_name}-ERROR"
+  message_retention_seconds   = 1209600
+  tags                        = "${local.tags}"
+}
+
+
+resource "aws_sqs_queue_policy" "sqs_error_queue_policy" {
+  queue_url = "${aws_sqs_queue.sqs_error_queue.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.sqs_error_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sqs_queue.sqs_queue.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
+
+
 resource "aws_sqs_queue" "sqs_queue" {
   name                          = "${var.sqs_name}"
   message_retention_seconds     = "${var.message_retention_seconds}"
@@ -31,40 +65,6 @@ resource "aws_sqs_queue_policy" "sqs_sns_policy" {
       "Condition": {
         "ArnEquals": {
           "aws:SourceArn": "${aws_sns_topic.replay_sns.arn}"
-        }
-      }
-    }
-  ]
-}
-POLICY
-}
-
-#ERROR QUEUE
-
-resource "aws_sqs_queue" "sqs_error_queue" {
-  name                        = "${var.sqs_name}-ERROR"
-  message_retention_seconds   = 1209600
-  tags                        = "${local.tags}"
-}
-
-
-resource "aws_sqs_queue_policy" "sqs_error_queue_policy" {
-  queue_url = "${aws_sqs_queue.sqs_error_queue.id}"
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "sqspolicy",
-  "Statement": [
-    {
-      "Sid": "First",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "sqs:SendMessage",
-      "Resource": "${aws_sqs_queue.sqs_error_queue.arn}",
-      "Condition": {
-        "ArnEquals": {
-          "aws:SourceArn": "${aws_sqs_queue.sqs_queue.arn}"
         }
       }
     }
