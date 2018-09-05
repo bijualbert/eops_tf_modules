4e8b1eb7650ad6fcf238553dc89cd599ed961f31
Antonio Neto <antonio.bengelsdorff@gmail.com>
2018-09-05 14:47:56 +0200

    Add environment variables to lambda

--- apps/lambda_function_scheduled_vpc/lambda.tf
+++ apps/lambda_function_scheduled_vpc/lambda.tf
@@ -8,6 +8,9 @@ resource "aws_lambda_function" "app" {
   runtime = "${var.runtime}"
   memory_size = "${var.memory_size}"
   timeout = "${var.timeout}"
+  environment {
+    variables = "${var.variables}"
+  }
   vpc_config = {
     subnet_ids = ["${split( ",", var.private == 1 ?   join(",", module.aws_core_data.private_subnets) :  join(",", concat(module.aws_core_data.private_subnets,module.aws_core_data.public_subnets)))}"]
     security_group_ids = ["${aws_security_group.sg_for_app.id}"]
