1de775f0b807bf27791b4d0aaaf6e3f035a0c887
Unknown <jeroen.devroede@albelli.com>
2018-11-01 16:08:38 +0100

    remove unused module

--- apps/cloudwatch_lambda/main.tf
+++ apps/cloudwatch_lambda/main.tf
@@ -7,9 +7,11 @@
 //  version = "~> 1.0"
 //}
 
+/*
 module "aws_core_data" {
   source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
    providers = {
     aws = "aws.default"
   }
 }
+*/
