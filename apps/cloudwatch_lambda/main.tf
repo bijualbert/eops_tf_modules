e91be3e13b49806f63bc363cf2205e8b2dffcf28
Unknown <jeroen.devroede@albelli.com>
2018-11-01 15:46:26 +0100

    Remove providers from modules

--- apps/cloudwatch_lambda/main.tf
+++ apps/cloudwatch_lambda/main.tf
@@ -1,11 +1,12 @@
-provider "aws" {
-  region = "${var.region}"
-}
-provider "aws" { 
-  region = "${var.region}" 
-  alias = "default" 
-  version = "~> 1.0" 
-}
+//provider "aws" {
+//  region = "${var.region}"
+//}
+//provider "aws" {
+//  region = "${var.region}"
+//  alias = "default"
+//  version = "~> 1.0"
+//}
+
 module "aws_core_data" {
   source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
    providers = {
--- data_providers/local-get-data/main.tf
+++ data_providers/local-get-data/main.tf
@@ -1,6 +1,6 @@
 # Terraform project
-
-provider "aws" {
-  region  = "${var.region}"
-//  profile = "${var.aws_profile}"
-}
+//
+//provider "aws" {
+//  region  = "${var.region}"
+////  profile = "${var.aws_profile}"
+//}
