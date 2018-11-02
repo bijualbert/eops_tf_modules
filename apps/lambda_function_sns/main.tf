f355daf53373fd9b7bb5abb254b142259a8a4d41
Antonio Neto <antonio.bengelsdorff@gmail.com>
2018-11-02 16:11:54 +0100

    add provider

--- apps/lambda_function_sns/main.tf
+++ apps/lambda_function_sns/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  alias = "default"
+}
+
 module "aws_core_data" {
   source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
   // For local development use instead:
