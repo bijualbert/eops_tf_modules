267eecc51d54ed888a980c2f67e0db367b3c5ecd
Antonio Neto <antonio.bengelsdorff@gmail.com>
2018-11-02 13:22:26 +0100

    add provider

--- apps/sns_topic_subscription/sns-topic-subscription.tf
+++ apps/sns_topic_subscription/sns-topic-subscription.tf
@@ -2,4 +2,7 @@ resource "aws_sns_topic_subscription" "topic-subscription" {
   topic_arn = "${var.topic_arn}"
   protocol  = "${var.protocol}"
   endpoint  = "${var.endpoint}"
+  providers = {
+    aws = "aws.default"
+  }
 }
