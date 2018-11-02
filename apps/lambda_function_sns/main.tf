981a4cdbd961843661b136bd772c650a825437fc
Antonio Neto <antonio.bengelsdorff@gmail.com>
2018-11-02 10:49:01 +0100

    add provider aws

--- apps/lambda_function_sns/main.tf
+++ apps/lambda_function_sns/main.tf
@@ -1,6 +1,6 @@
-//provider "aws" {
-//  region = "${var.region}"
-//}
+provider "aws" {
+  region = "${var.region}"
+}
 
 module "aws_core_data" {
   source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
