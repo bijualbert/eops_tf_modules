3cf748197def26ed6ba5db9deb1aeafd15585e69
gmariano <giuseppe.mariano@albelli.com>
2018-11-06 16:32:08 +0100

    remove unused provider

--- apps/sns_topic/main.tf
+++ apps/sns_topic/main.tf
@@ -5,9 +5,9 @@ provider "aws" {
   alias  = "default"
 }
 
-provider "aws" {
-  alias  = "us-east-1"
-}
+# provider "aws" {
+#   alias  = "us-east-1"
+# }
 
 module "aws_core_data" {
   source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
