582fd2187ef96a21cd12fd96e15109b3113fc6b8
gmariano <giuseppe.mariano@albelli.com>
2018-11-06 13:43:58 +0100

    Add aws_core_data to lambda_cloudwatch

--- apps/cloudwatch_lambda/main.tf
+++ apps/cloudwatch_lambda/main.tf
@@ -8,3 +8,10 @@
 # provider "aws" {
 #   alias  = "us-east-1"
 # }
+
+module "aws_core_data" {
+  source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
+  # providers = { 
+  #  aws = "aws.default" 
+  # } 
+}
