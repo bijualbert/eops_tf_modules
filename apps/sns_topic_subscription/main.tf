f1c7d8688a864e334c438a3feaa4edf2b7a22aba
gmariano <giuseppe.mariano@albelli.com>
2018-11-06 11:16:02 +0100

    remove reference to aws_core_data

--- apps/sns_topic_subscription/main.tf
+++ apps/sns_topic_subscription/main.tf
@@ -8,12 +8,12 @@ provider "aws" {
 }
 
 
-module "aws_core_data" {
-  source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
-  providers = { 
-   aws = "aws.default" 
-  } 
-}
+# module "aws_core_data" {
+#   source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
+#   providers = { 
+#    aws = "aws.default" 
+#   } 
+# }
 
 // For local development use instead:
 //module "aws_core_data" {
