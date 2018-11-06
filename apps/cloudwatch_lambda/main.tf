4228b4583ff4791adf26d8e51ea5c5969ec48338
gmariano <giuseppe.mariano@albelli.com>
2018-11-06 15:44:22 +0100

    Revert "try to use the explicit way to pass providers"
    This reverts commit 06d62b6b1c42df29b9c9ebc4ae05ff7530c8f81e.

--- apps/cloudwatch_lambda/main.tf
+++ apps/cloudwatch_lambda/main.tf
@@ -1,17 +1,17 @@
-provider "aws" {
-}
+# provider "aws" {
+# }
 
-provider "aws" {
-  alias  = "default"
-}
+# provider "aws" {
+#   alias  = "default"
+# }
 
-provider "aws" {
-  alias  = "us-east-1"
-}
+# provider "aws" {
+#   alias  = "us-east-1"
+# }
 
 module "aws_core_data" {
   source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
-  providers = { 
-   aws = "aws.default" 
-  } 
+  # providers = { 
+  #  aws = "aws.default" 
+  # } 
 }
