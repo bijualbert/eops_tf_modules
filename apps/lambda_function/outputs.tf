1c18e5a06af8da5e7b4980dfe83aa410a21aee60
gmariano <giuseppe.mariano@albelli.com>
2019-01-22 15:56:59 +0100

    fix output variables

--- apps/lambda_function/outputs.tf
+++ apps/lambda_function/outputs.tf
@@ -3,17 +3,17 @@ output "lambda_arn" {
 }
 
 output "lambda_name" {
-  value = "${aws_lambda_function.app.function_name}"
+  value = "${aws_lambda_function.app.*.function_name}"
 }
 
 output "iam_role_arn" {
-  value = "${aws_iam_role.iam_for_app.arn}"
+  value = "${aws_iam_role.iam_for_app.*.arn}"
 }
 
 output "invokation_arn" {
-  value = "${aws_lambda_function.app.invoke_arn}"
+  value = "${aws_lambda_function.app.*.invoke_arn}"
 }
 
 output "authorization_arn" {
-  value = "${aws_lambda_function.app.arn}"
+  value = "${aws_lambda_function.app.*.arn}"
 }
