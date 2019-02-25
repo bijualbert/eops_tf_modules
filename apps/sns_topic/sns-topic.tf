794a41212824d5c5c6a2151104680f157ec4d4c9
gmariano <giuseppe.mariano@albelli.com>
2019-02-25 16:23:10 +0100

    MAM-166 make SNS policy inline

--- apps/sns_topic/sns-topic.tf
+++ apps/sns_topic/sns-topic.tf
@@ -4,35 +4,31 @@ resource "aws_sns_topic" "sns-topic" {
 
 resource "aws_sns_topic_policy" "default" {
   arn = "${aws_sns_topic.sns-topic.arn}"
-}
 
-
-resource "aws_sns_topic_policy" "sns-topic-policy" {
-  arn = "${aws_sns_topic.sns-topic.arn}"
   policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "${var.app_name}-policy",
-  "Statement": [
-    {
-      "Sid": "Default",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": [
-        "SNS: Subscribe",
-        "SNS: SetTopicAttributes",
-        "SNS: RemovePermission",
-        "SNS: Receive",
-        "SNS: Publish",
-        "SNS: ListSubscriptionsByTopic",
-        "SNS: GetTopicAttributes",
-        "SNS: DeleteTopic",
-        "SNS: AddPermission"],
-      "Resource": "${aws_sns_topic.sns-topic.arn}"
-    }${var.sns_extra_policy_statements}
-  ]
-}
-POLICY
+  {
+    "Version": "2012-10-17",
+    "Id": "${var.app_name}-policy",
+    "Statement": [
+      {
+        "Sid": "Default",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "*"
+        },
+        "Action": [
+          "SNS: Subscribe",
+          "SNS: SetTopicAttributes",
+          "SNS: RemovePermission",
+          "SNS: Receive",
+          "SNS: Publish",
+          "SNS: ListSubscriptionsByTopic",
+          "SNS: GetTopicAttributes",
+          "SNS: DeleteTopic",
+          "SNS: AddPermission"],
+        "Resource": "${aws_sns_topic.sns-topic.arn}"
+      }${var.sns_extra_policy_statements}
+    ]
+  }
+  POLICY
 }
