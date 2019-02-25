8a0259cda060f6cfff50f4bc0f2e34b511ab8653
gmariano <giuseppe.mariano@albelli.com>
2019-02-25 18:46:54 +0100

    MAM-166 Clean code

--- apps/sns_topic/sns-topic.tf
+++ apps/sns_topic/sns-topic.tf
@@ -17,18 +17,39 @@ resource "aws_sns_topic_policy" "default" {
           "AWS": "*"
         },
         "Action": [
-          "SNS: Subscribe",
-          "SNS: SetTopicAttributes",
-          "SNS: RemovePermission",
-          "SNS: Receive",
-          "SNS: Publish",
-          "SNS: ListSubscriptionsByTopic",
-          "SNS: GetTopicAttributes",
-          "SNS: DeleteTopic",
-          "SNS: AddPermission"],
+          "SNS:Subscribe",
+          "SNS:SetTopicAttributes",
+          "SNS:RemovePermission",
+          "SNS:Receive",
+          "SNS:Publish",
+          "SNS:ListSubscriptionsByTopic",
+          "SNS:GetTopicAttributes",
+          "SNS:DeleteTopic",
+          "SNS:AddPermission"],
         "Resource": "${aws_sns_topic.sns-topic.arn}"
       }${var.sns_extra_policy_statements}
     ]
   }
   POLICY
 }
+
+# resource "aws_sns_topic_policy" "sns_photo_extraction_done_policy" {
+#   arn = "${module.sns_photo_extraction_done.sns-topic-arn}"
+#   policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Id": "policy1",
+#   "Statement": [
+#     {
+#       "Sid": "First",
+#       "Effect": "Allow",
+#       "Principal": {
+#         "AWS": "arn:aws:iam::${var.PO_legacy_account}:root"
+#       },
+#       "Action": "SNS:Publish",
+#       "Resource": "${module.sns_photo_extraction_done.sns-topic-arn}"
+#     }
+#   ]
+# }
+# POLICY
+# }
