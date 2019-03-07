913d968f80ec2d7e86d888beb7aae681a7a6027d
Martijn Mol <martijn.mol@albelli.com>
2019-03-07 11:58:40 +0100

    MAM-274 add default parameters

--- apps/sqs_queue/variables.tf
+++ apps/sqs_queue/variables.tf
@@ -13,3 +13,28 @@ variable "extra_policy_statements"
 variable "sns_alert_arn" {
   default = ""
 }
+variable "description"
+{
+  default = ""
+}
+variable "receive_wait_time_seconds" {
+  default = "20"
+}
+variable "message_retention_seconds" {
+  default = "1209600"
+}
+variable "visibility_timeout_seconds" {
+  default = 120
+}
+variable "redrive_policy_retry_count" {
+  default = 3
+}
+variable "tags_business_unit" {
+  default = "Albumprinter"
+}
+variable "tags_cost_center" {
+  default = "CC443100"
+}
+variable "tags_team" {}
+variable "tags_purpose" {}
+variable "environment" {}
