caf86c4859570ea7b2dccc6855144cb62a6410c5
Antonio Neto <antonio.bengelsdorff@gmail.com>
2019-06-12 15:40:36 +0200

    MAM-749 new module lambda_fuction_sqs_no_vpc

---
+++ apps/lambda_function_sqs_vpc/cloudwatch.tf
@@ -0,0 +1,88 @@
+module "lambda_app_cloudwatch" {
+  //source = "git@github.com:albumprinter/eops_tf_modules.git//apps/cloudwatch_lambda"
+  source = "../../apps/cloudwatch_lambda"
+  app_name = "${var.app_name}"
+  log_group_name = "/aws/lambda/${var.app_name}"
+  alarm_threshold = "${var.lambda_memory_alert_threshold}"
+  alarm_action_arn = "${var.alarm_action_arn}"
+  environment = "${var.environment}"
+  tags_team = "${var.tags_team}"
+  tags_purpose = "${var.tags_purpose}"
+  description = "${var.description}"
+  retention_days = "${var.retention_days}"  
+  providers = {
+   aws = "aws"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "stale_messages_alarm" {
+  alarm_name                = "${var.app_name}-STALE-MESSAGES-ALARM"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "ApproximateAgeOfOldestMessage"
+  namespace                 = "AWS/SQS"
+  period                    = "3600"  // 1 hour
+  statistic                 = "Maximum"
+  threshold                 = "3600" // 1 hour
+  datapoints_to_alarm       = "1"
+  alarm_actions             = ["${var.alarm_action_arn}"]
+  count                     = "${var.enable_cloudwatch_alarms}"
+  dimensions {
+    QueueName = "${aws_sqs_queue.sqs_queue.name}"
+  }
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "error_messages_alarm_1" {
+  alarm_name                = "${var.app_name}-ERROR-MESSAGES-ALARM-1"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "ApproximateAgeOfOldestMessage"
+  namespace                 = "AWS/SQS"
+  period                    = "60"  // 1 minute
+  statistic                 = "Minimum"
+  threshold                 = "60" // 1 minute
+  treat_missing_data        = "notBreaching"
+  datapoints_to_alarm       = "1"
+  alarm_actions             = ["${var.alarm_action_arn}"]
+  count                     = "${var.enable_cloudwatch_alarms}"
+  dimensions {
+    QueueName = "${aws_sqs_queue.sqs_error_queue.name}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "error_messages_alarm_2" {
+  alarm_name                = "${var.app_name}-ERROR-MESSAGES-ALARM-2"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "ApproximateAgeOfOldestMessage"
+  namespace                 = "AWS/SQS"
+  period                    = "3600"  // 1 hour
+  statistic                 = "Maximum"
+  threshold                 = "3600" // 1 hour
+  treat_missing_data        = "notBreaching"
+  datapoints_to_alarm       = "1"
+  alarm_actions             = ["${var.alarm_action_arn}"]
+  count                     = "${var.enable_cloudwatch_alarms}"
+  dimensions {
+    QueueName = "${aws_sqs_queue.sqs_error_queue.name}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "error_messages_alarm_3" {
+  alarm_name                = "${var.app_name}-ERROR-MESSAGES-ALARM-3"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "ApproximateNumberOfMessagesVisible"
+  namespace                 = "AWS/SQS"
+  period                    = "900"  // 15 minutes
+  statistic                 = "Maximum"
+  threshold                 = "10" // 1 hour
+  treat_missing_data        = "notBreaching"
+  datapoints_to_alarm       = "1"
+  alarm_actions             = ["${var.alarm_action_arn}"]
+  count                     = "${var.enable_cloudwatch_alarms}"
+  dimensions {
+    QueueName = "${aws_sqs_queue.sqs_error_queue.name}"
+  }
+}
---
+++ apps/lambda_function_sqs_vpc/lambda.tf
@@ -0,0 +1,97 @@
+resource "aws_lambda_function" "app" {
+  s3_bucket = "${var.lambda_bucket_name}"
+  s3_key = "builds/lambda/${var.app_name}/lambda.zip"
+  function_name = "${var.app_name}"
+  description = "${var.description}"
+  role = "${aws_iam_role.iam_for_app.arn}"
+  handler = "${var.handler}"
+  runtime = "${var.runtime}"
+  memory_size = "${var.memory_size}"
+  timeout = "${var.timeout}"
+  vpc_config = {
+    subnet_ids = ["${split( ",", var.private == 1 ?   join(",", module.aws_core_data.private_subnets) :  join(",", concat(module.aws_core_data.private_subnets,module.aws_core_data.public_subnets)))}"]
+    security_group_ids = ["${aws_security_group.sg_for_app.id}"]
+  }
+  environment {
+    variables = "${var.variables}"
+  }
+  tracing_config {
+    mode = "${var.tracing_config}"
+  }
+  count            = "${var.enabled}"
+  tags = "${local.tags}"
+  reserved_concurrent_executions = "${var.reserved_concurrent_executions}"
+}
+
+resource "aws_lambda_event_source_mapping" "event_source_mapping" {
+  batch_size        = 1
+  event_source_arn  = "${aws_sqs_queue.sqs_queue.arn}"
+  function_name     = "${aws_lambda_function.app.arn}"
+  enabled           = "${var.event_source_mapping_active}"
+}
+
+resource "aws_iam_role" "iam_for_app" {
+  name = "${var.app_name}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "1",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    },
+    {
+      "Sid": "2",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "iam_policy_for_app" {
+  name = "${var.app_name}"
+  role = "${aws_iam_role.iam_for_app.id}"
+  policy = "${var.iam_policy_document}"
+}
+
+resource "aws_security_group" "sg_for_app" {
+  name = "${var.app_name}"
+  description = "Allow all inbound traffic for the scheduled lambda function"
+  vpc_id = "${module.aws_core_data.vpc_id}"
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "185.184.204.70/32",
+      "62.97.245.10/32",
+      "185.184.204.74/32",
+      "62.102.226.22/32",
+      "213.41.124.76/32",
+      "77.60.83.148/32",
+      "62.21.226.193/32",
+      "192.168.0.0/16",
+      "10.0.0.0/8",
+      "172.16.0.0/12"
+    ]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${local.tags}"
+}
