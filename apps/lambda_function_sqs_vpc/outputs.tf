e7053e1428c757cae45aba72d10af068e848ff6e
Antonio Neto <antonio.bengelsdorff@gmail.com>
2019-06-13 13:38:09 +0200

    MAM-749 add output lambda_security_group

--- apps/lambda_function_sqs_vpc/outputs.tf
+++ apps/lambda_function_sqs_vpc/outputs.tf
@@ -8,3 +8,8 @@ output "sqs_queue_arn" {
 output "sqs_queue_url" {
   value = "${aws_sqs_queue.sqs_queue.id}"
 }
+
+output "lambda_security_group" {
+  description = "SG id created for the lambda"
+  value = "${aws_security_group.sg_for_app.id}"
+}
