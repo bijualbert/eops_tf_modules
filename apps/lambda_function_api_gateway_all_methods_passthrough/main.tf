01a652f90704b3c2643c45a43c39156badb8f310
Jeroen de Vroede <jeroen.devroede@albelli.com>
2019-07-25 14:34:25 +0200

    removing provider from module

--- apps/lambda_function_api_gateway_all_methods_passthrough/main.tf
+++ apps/lambda_function_api_gateway_all_methods_passthrough/main.tf
@@ -1,6 +1,3 @@
-provider "aws" {
-}
-
 module "aws_core_data" {
   //source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
   source       = "../../data_providers/shared-vpc-data"
