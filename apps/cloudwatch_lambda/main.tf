f2af602d4540cdcf5b90dd2d9e5f6dea7120abdf
Alejandro Vallecilla <avallecillac@gmail.com>
2019-08-20 15:58:21 +0200

    Removes unused data source

--- apps/cloudwatch_lambda/main.tf
+++ apps/cloudwatch_lambda/main.tf
@@ -1,11 +1,2 @@
 provider "aws" {
 }
-
-
-module "aws_core_data" {
-  //source = "git@github.com:albumprinter/eops_tf_modules.git//data_providers/aws_account_core_data"
-  source = "../../data_providers/aws_account_core_data"
-  providers = {
-   aws = "aws"
-  } 
-}
