743661a16fb671503fb815afecc0d2b4351cc616
Phil Jory <philjory@hotmail.com>
2019-11-20 15:16:33 +0100

    Add more outputs for the sqs sns subscription

--- apps/sqs_consume_sns/outputs.tf
+++ apps/sqs_consume_sns/outputs.tf
@@ -1,7 +1,18 @@
 output "sqs_queue_arn" {
   value = "${aws_sqs_queue.sqs_queue.arn}"
 }
+output "sqs_queue_id" {
+  value = "${aws_sqs_queue.sqs_queue.id}"
+}
+
 
 output "sqs_error_queue_arn" {
   value = "${aws_sqs_queue.sqs_error_queue.arn}"
+}
+output "sqs_error_queue_id" {
+  value = "${aws_sqs_queue.sqs_error_queue.id}"
+}
+
+output "replay_sns_arn" {
+  value = "${aws_sns_topic.replay_sns.arn}"
 }
