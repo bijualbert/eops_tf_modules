b25adbd358115b41036a4bb12796ba6d5f31b435
Antonio Neto <antonio.neto@albellicom>
2019-12-20 13:46:59 +0100

    revert expose lambda function name

--- apps/lambda_function_scheduled_vpc/outputs.tf
+++ apps/lambda_function_scheduled_vpc/outputs.tf
@@ -1,9 +1,6 @@
 output "lambda_arn" {
   value = "${aws_lambda_function.app.arn}"
 }
-output "function_name" {
-  value = "${aws_lambda_function.app.function_name}"
-}
 output "iam_role_arn" {
   value = "${aws_iam_role.iam_for_app.arn}"
 }
--- apps/lambda_function_sns/outputs.tf
+++ apps/lambda_function_sns/outputs.tf
@@ -7,9 +7,6 @@ output "lambda_kms_key_id" {
 output "lambda_arn" {
   value = "${aws_lambda_function.app.*.arn}"
 }
-output "function_name" {
-  value = "${aws_lambda_function.app.function_name}"
-}
 
 output "iam_role_arn" {
   value = "${aws_iam_role.iam_for_app.*.arn}"
