ec8f3b7a4e2b5503444c7c7abb41dcda0c661f38
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2021-08-09 11:39:20 +0200

    BEAM-3037 fix

--- apps/lambda_function_s3_no_vpc/lambda.tf
+++ apps/lambda_function_s3_no_vpc/lambda.tf
@@ -65,7 +65,7 @@ resource "aws_iam_role_policy" "iam_policy_for_app" {
 resource "aws_lambda_permission" "allow_bucket" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.func.arn
+  function_name = "${aws_lambda_function.func.arn}"
   principal     = "s3.amazonaws.com"
   source_arn    = var.s3_bucket_arn
 }
@@ -74,7 +74,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = var.s3_bucket_name
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.func.arn
+    lambda_function_arn = "${aws_lambda_function.func.arn}"
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "AWSLogs/"
     filter_suffix       = ".log"
