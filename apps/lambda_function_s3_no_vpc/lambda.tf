15dca12aa84ba9a9ae55b7ed45672a50e0d60326
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2021-08-10 10:26:06 +0200

    BEAM-3037 remove commented code

--- apps/lambda_function_s3_no_vpc/lambda.tf
+++ apps/lambda_function_s3_no_vpc/lambda.tf
@@ -79,6 +79,4 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
     filter_prefix       = "${var.s3_filter_prefix}"
     filter_suffix       = "${var.s3_filter_suffix}"
   }
-
-  # depends_on = ["aws_lambda_permission.allow_bucket"]
 }
